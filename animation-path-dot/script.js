c1d583e88c9eff27fb4abff74d064cdcb289eed9
Magno Silva <magnosansil@outlook.com>
2025-02-11 15:01:53 -0300

    New Animations

--- animation-path-dot/script.js
+++ animation-path-dot/script.js
@@ -3,24 +3,21 @@ const circle = document.getElementById('movingCircle');
 const pathLength = path.getTotalLength();
 
 function animateCircle() {
-  const animationDuration = 10000; // mesma duração que a animação CSS
+  const animationDuration = 10000;
   const start = performance.now();
 
   function step(timestamp) {
     const elapsed = timestamp - start;
-    
-    // Verifica se a animação já foi concluída
     if (elapsed < animationDuration) {
       const progress = elapsed / animationDuration;
 
-      // Calcula o ponto atual ao longo do caminho
       const point = path.getPointAtLength(progress * pathLength);
       circle.setAttribute('cx', point.x);
       circle.setAttribute('cy', point.y);
 
-      requestAnimationFrame(step); // Continua a animação
+      requestAnimationFrame(step); 
     } else {
-      // Garante que o círculo esteja na última posição ao final da animação
+
       const point = path.getPointAtLength(pathLength);
       circle.setAttribute('cx', point.x);
       circle.setAttribute('cy', point.y);
@@ -30,5 +27,4 @@ function animateCircle() {
   requestAnimationFrame(step);
 }
 
-// Inicia a animação
 animateCircle();
